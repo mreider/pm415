f8b31d5245bb97b78d221c24100098b066649aa2
Alex Xela <nosuchip@gmail.com>
2018-09-17 14:33:43 +0700

    Fix columns names

--- models/role.js
+++ models/role.js
@@ -2,10 +2,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Role = sequelize.define('Role', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    role: { type: DataTypes.STRING, allowNull: false }
-  }, {
-    underscored: true
-  });
+    role: { type: DataTypes.STRING, allowNull: false },
+    createdAt: { field: 'created_at', type: DataTypes.DATE },
+    updatedAt: { field: 'updated_at', type: DataTypes.DATE }
+  }, {});
 
   Role.associate = function(models) {
     Role.belongsToMany(models.User, {
