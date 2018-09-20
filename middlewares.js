607188d6115c3a4ffdeeaedeb58474f5b1fb6a93
Alex Xela <nosuchip@gmail.com>
2018-09-20 10:55:35 +0700

    Add future middleware for check user role in org

--- middlewares.js
+++ middlewares.js
@@ -1,18 +1,16 @@
 
 const Jwt = require('jsonwebtoken');
-// const Url = require('url');
+const Url = require('url');
 const Config = require('./config');
 const User = require('./models/user');
 const Organization = require('./models/organization');
 
 module.exports = {
   UserTokenMiddleware: () => {
-    return async (req, res, next) => {       
-      //console.log("+", req.token);
+    return async (req, res, next) => {
       if (!req.token) return next(null);
 
       try {
-        
         const decoded = Jwt.verify(req.token, Config.appKey);
         const [user, organization] = await Promise.all([
           User.where({id: decoded.userId}).fetch({withRelated: ['organizations']}),
@@ -21,7 +19,6 @@ module.exports = {
 
         req.user = user;
         req.organization = organization;
-
 
         next(null);
       } catch (error) {
@@ -42,21 +39,26 @@ module.exports = {
     next(null);
   },
 
-  // AdminRequired: (req, res, next) => {
-  //   var redirect = req.headers.referer ? Url.parse(req.headers.referer).path : '/';
+  OrgAdminRequired: (req, res, next) => {
+    var redirect = req.headers.referer ? Url.parse(req.headers.referer).path : '/';
 
-  //   if (!req.user) {
-  //     return res.boom.unauthorized('Authentication required', {success: false, redirect: '/login?next=' + redirect});
-  //   }
+    if (!req.user) {
+      return res.boom.unauthorized('Authentication required', {success: false, redirect: '/login?next=' + redirect});
+    }
 
-  //   if (!req.user.get('isActive') || !req.user.get('confirmedAt')) {
-  //     return res.boom.forbidden('User not confirmed or inactive', {success: false});
-  //   }
+    if (!req.user.get('isActive') || !req.user.get('confirmedAt')) {
+      return res.boom.forbidden('User not confirmed or inactive', {success: false});
+    }
 
-  //   if (!req.user.hasRole(User.AdminRole)) {
-  //     return res.boom.unauthorized('Admin privileges required', {success: false, redirect: '/login?next=' + redirect});
-  //   }
+    if (!req.organization) {
+      return res.boom.forbidden('User must be assigned to organization first', {success: false});
+    }
 
-  //   next(null);
-  // }
+    // TODO: Add role in organization check here instead of user role check
+    // if (!req.user.hasRole(User.AdminRole)) {
+    //   return res.boom.unauthorized('Admin privileges required', {success: false, redirect: '/login?next=' + redirect});
+    // }
+
+    next(null);
+  }
 };
