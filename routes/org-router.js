0161f613e162857beab33679017db980fa73d8e1
Alex Xela <nosuchip@gmail.com>
2018-09-20 11:01:52 +0700

    Fix style

--- routes/org-router.js
+++ routes/org-router.js
@@ -4,15 +4,13 @@ const middlewares = require('../middlewares');
 const Organization = require('../models/organization');
 const OmitDeep = require('omit-deep');
 
+router.get('/', middlewares.LoginRequired, function(req, res) {
+  const user = OmitDeep(req.user.toJSON(), ['password']);
 
-router.get('/', middlewares.LoginRequired, function(req, res) { 
-  const user = OmitDeep(req.user.toJSON(), ['password']);	
-  
-  current = ({org_id : 1, org_name : 'company name', admins : [1,2,3,4], authorized : [1,2],  verified: [1], backlogs : [1,2], strategic_i : [11,233,44] });	
+  current = ({org_id : 1, org_name : 'company name', admins : [1,2,3,4], authorized : [1,2],  verified: [1], backlogs : [1,2], strategic_i : [11,233,44] });
+
   res.json({success: true, data: user.organizations, current: current})
-  });
-//   res.json();
- 
+});
 
 router.post('/switch/:organizationId', middlewares.LoginRequired, async (req, res) => {
   const organizationId = parseInt(req.params.organizationId);
