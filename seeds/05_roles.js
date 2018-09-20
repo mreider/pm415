9bcfc13d342bd4a09fabe3bebad9648b0bfcde10
Alex Xela <nosuchip@gmail.com>
2018-09-20 11:16:06 +0700

    Refactor seeds

---
+++ seeds/05_roles.js
@@ -0,0 +1,10 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('roles').del()
+    .then(function () {
+      return knex('roles').insert([
+        {id: 1, role: 'Member'},
+        {id: 2, role: 'Admin'}
+      ]);
+    });
+};
