6810cad8737ab1c8dd58b75667806788effc4043
Alex Xela <nosuchip@gmail.com>
2018-09-24 02:51:58 +0700

    Fix linter warns

--- seeds/20_users-to-organizations.js
+++ seeds/20_users-to-organizations.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
         { user_id: 1, organization_id: 2, role_id: 2 },
         { user_id: 2, organization_id: 1, role_id: 2 },
         { user_id: 2, organization_id: 2, role_id: 1 },
-        { user_id: 2, organization_id: 3, role_id: 1 }    
+        { user_id: 2, organization_id: 3, role_id: 1 }
       ]);
     });
 };
