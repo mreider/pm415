a493691ea3978931c9370e68ca51bfe82ae36bb3
Alex Xela <nosuchip@gmail.com>
2018-09-24 01:31:16 +0700

    Add models serialization with dropping service fields.

--- utils.js
+++ utils.js
@@ -1,6 +1,20 @@
-module.exports = {
-  lowercase: str => {
-    if (!str) return str;
-    return str.toLowerCase();
-  }
+const OmitDeep = require('omit-deep');
+
+module.exports = exports = {};
+
+exports.serialize = function(obj) {
+  if (typeof obj.toJSON === 'function') obj = obj.toJSON();
+
+  const serialized = OmitDeep(obj, [
+    'password',
+    'isActive',
+    'confirmedAt',
+    'createdAt',
+    'updatedAt',
+    '_pivot_user_id',
+    '_pivot_organization_id',
+    '_pivot_role_id'
+  ]);
+
+  return serialized;
 };
