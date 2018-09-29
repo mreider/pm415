29a1a11390b23eb8ac3757156c5dd14e65730070
shamash <alexander.prilipko@gmail.com>
2018-09-30 01:40:59 +0400

    added sending mail to admins orgs after user verification

--- routes/render-router.js
+++ routes/render-router.js
@@ -1,8 +1,16 @@
 const Express = require('express');
-
 const User = require('../models/user');
-
 const router = Express.Router();
+const knex = require('../db').knex;
+const Role = require('../models/role');
+const UORole = require('../models/users_organizations_roles');
+const Config = require('../config');
+const Nodemailer = require('nodemailer');
+const SendGridTransport = require('nodemailer-sendgrid-transport');
+const Handlebars = require('nodemailer-express-handlebars');
+const mailer = Nodemailer.createTransport(SendGridTransport(Config.mailerConfig));
+const Utils = require('../utils');
+mailer.use('compile', Handlebars(Config.mailerConfig.rendererConfig));
 
 router.get('/verify', async (req, res) => {
   const token = req.query.token;
@@ -12,11 +20,42 @@ router.get('/verify', async (req, res) => {
 
   const user = await User.where({ id: validated.data.userId }).fetch();
   if (!user) return res.send('User not found.');
-
   user.set({ isActive: true, confirmedAt: new Date() });
   await user.save();
+  // send mail admins
+  if (validated.data.organization) {
+    const uorole = await UORole.where('role_id', '<>', Role.PendingRoleId).where({ user_id: Number(validated.data.userId), organization_id: Number(validated.data.organization) }).fetch();
+    if (uorole) return res.send('already confirmed');
+    let users = await knex('users_organizations_roles').select('u.email', 'o.name as organization')
+      .leftJoin('users as u', 'users_organizations_roles.user_id', 'u.id')
+      .leftJoin('organizations as o', 'users_organizations_roles.organization_id', 'o.id')
+      .where({ organization_id: validated.data.organization, role_id: Role.AdminRoleId, 'is_active': 1 });
+    let data = {};
+    if (user.get('firstName')) data.firstName = user.get('firstName');
+    if (user.get('lastName')) data.lastName = user.get('lastName');
+    if (user.get('email')) data.email = user.get('email');
+    users = Utils.serialize(users);
+    users.createdUser = data;
+    users.forEach(sendMail, data);
+  }
 
   res.send('Your account confirmed successfully.');
 });
+
+function sendMail(value) {
+  var mail = {
+    from: Config.mailerConfig.from,
+    to: value.email,
+    subject: 'Authorization pending for' + this.firstName + ' ' + this.lastName,
+    template: 'admin-info_pending',
+    context: {
+      firstName: this.firstName,
+      lastName: this.firstName,
+      email: this.email,
+      organization: value.organization
+    }
+  };
+  mailer.sendMail(mail);
+}
 
 module.exports = router;
