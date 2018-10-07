829d7c753517aeb5310f491ec3044fdc605e6d31
Alex Xela <nosuchip@gmail.com>
2018-10-08 03:22:04 +0700

    Refactoring and cleanup

--- validation.js
+++ validation.js
@@ -37,29 +37,31 @@ const RegisterSchema = {
 const VerifySchema = {
   token: Joi.string().required()
 };
+
 const ForgotPasswordSchema = {
   email: Joi.string().email().required().label('Email')
 };
+
 const NewOrganizationSchema = {
   name: Joi.string().min(2).required().label('Name')
 };
+
 const UpdateOrganizationSchema = {
   name: Joi.string().min(2).required().label('Name'),
-  organizationId: Joi.string().required().label('organizationId')
+  orgId: Joi.string().required().label('orgId')
 };
+
 const InviteLinkSchema = {
   name: Joi.string().min(2).required().label('Name'),
   email: Joi.string().email().required().label('Email')
 };
+
 const ChangePasswordSchema = {
   token: Joi.string().required().label('token'),
   password: Joi.string().required().label('Password'),
   confirmation: Joi.string().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } }).label('Confirmation')
 };
-const DeleteOrgSchema = {
-  userId: Joi.string().required().label('userId'),
-  organizationId: Joi.string().required().label('organizationId')
-};
+
 const UpdateUserSchema = {
   firstName: Joi.string().min(0).allow('').optional().label('Firstname'),
   lastName: Joi.string().optional().label('Lastname'),
@@ -67,6 +69,7 @@ const UpdateUserSchema = {
   password: Joi.string().optional(),
   confirmation: Joi.string().valid(Joi.ref('password')).optional().options({ language: { any: { allowOnly: 'must match password' } } }).label('Confirmation')
 };
+
 module.exports = {
   validate: validate,
   LoginSchema,
