6a2ff38bae048c5058e21fbbae3e426a84c9de68
shamash <alexander.prilipko@gmail.com>
2019-04-26 18:37:45 +0700

    added subowner and subownerid to route GET Subscribers part2

--- routes/subscribe-router.js
+++ routes/subscribe-router.js
@@ -8,10 +8,21 @@ const Subscribers = require('../models/subscribers');
 const { validate, SubscribersInserDeleteSchema } = require('../validation');
 
 // get subscribers id array of users
+router.get('/:ownerTable/:id/:subowner/:subownerId', [middlewares.LoginRequired], async (req, res) => {
+  const id = parseInt(req.params.id);
+  const ownerTable = req.params.ownerTable;
+  const subownerId = parseInt(req.params.subownerId);
+  const subowner = req.params.subowner;
+
+  let subscribers = await Subscribers.getSubscribers(ownerTable, id, subowner, subownerId);
+
+  return res.json({ success: true, subscribers });
+});
+
+// get subscribers id array of users
 router.get('/:ownerTable/:id', [middlewares.LoginRequired], async (req, res) => {
   const id = parseInt(req.params.id);
   const ownerTable = req.params.ownerTable;
-
   let subscribers = await Subscribers.getSubscribers(ownerTable, id);
 
   return res.json({ success: true, subscribers });
